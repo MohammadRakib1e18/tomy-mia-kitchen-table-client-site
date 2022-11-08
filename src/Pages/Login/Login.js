cd59786ab696fcdc2413db10bf1c5b564aa92d2e
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-11-09 03:05:36 +0600

    login functionalities added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,14 +1,62 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 import { FiEyeOff, FiEye } from "react-icons/fi";
+import toast from "react-hot-toast";
+import { AuthContext } from "../../contexts/AuthProvider";
+import Swal from "sweetalert2";
 
 const Login = () => {
   const [show, setShow] = useState(false);
+  const { signIn, googleSignIn, githubSignIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    signIn(email, password)
+      .then((result) => {
+        Swal.fire({
+          icon: "success",
+          title: `Login Successful!`,
+          showConfirmButton: true,
+          timer: 1500,
+        });
+        navigate('/');
+      })
+      .catch((error) => {
+       toast.error(`${error.message}`);
+      });
+  };
+
+  const handleGoogleSignIn = () => {
+    googleSignIn()
+      .then((result) => {
+        toast.success("SignIn with Google Successful!");
+        navigate("/");
+      })
+      .catch((error) => {
+        toast.error(`${error.message}`);
+      });
+  };
+  const handleGithubSignIn = () => {
+    githubSignIn()
+      .then((result) => {
+        toast.success("SignIn with Github Successful!");
+        navigate("/");
+      })
+      .catch((error) => {
+        toast.error(`${error.message}`);
+      });
+  };
   return (
     <div className="mt-8 w-5/6 mx-auto max-w-md p-8 space-y-3 rounded-xl   bg-slate-700   text-gray-100">
       <h1 className="text-2xl font-bold text-center">Sign in</h1>
       <form
+        onSubmit={handleLogin}
         className="space-y-6 ng-untouched ng-pristine ng-valid"
       >
         <div className="space-y-1 text-sm">
@@ -59,10 +107,16 @@ const Login = () => {
         <div className="flex-1 h-px sm:w-16   bg-gray-400"></div>
       </div>
       <div className="flex justify-between space-x-4">
-        <h2 className="bg-red-500 text-2xl w-full rounded-full flex justify-center py-2 cursor-pointer">
+        <h2
+          onClick={handleGoogleSignIn}
+          className="bg-red-500 text-2xl w-full rounded-full flex justify-center py-2 cursor-pointer"
+        >
           <FaGoogle />
         </h2>
-        <h2 className="bg-black text-2xl w-full rounded-full flex justify-center py-2 cursor-pointer">
+        <h2
+          onClick={handleGithubSignIn}
+          className="bg-black text-2xl w-full rounded-full flex justify-center py-2 cursor-pointer"
+        >
           <FaGithub />
         </h2>
       </div>
