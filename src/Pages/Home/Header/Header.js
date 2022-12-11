261422838cb93295a3896ac0272891c519f50073
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-12-11 22:16:52 +0600

    header text margin adjusted

--- src/Pages/Home/Header/Header.js
+++ src/Pages/Home/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className="banner-container h-1  text-slate-100">
       <Carousel>
-        <div className="ml-20 space-y-5">
+        <div className="ml-4 sm:ml-20 space-y-5">
           <h1 className="text-xl md:text-4xl md:w-5/6 w-2/3 font-bold">
             To live a full life, you have to fill your stomach first.
           </h1>
@@ -22,7 +22,7 @@ const Header = () => {
             </button>
           </Link>
         </div>
-        <div className="ml-20 space-y-5">
+        <div className="ml-4 sm:ml-20 space-y-5">
           <h1 className="text-xl md:text-4xl md:w-5/6 w-2/3 font-bold">
             First, we eat. Then, we do everything else
           </h1>
@@ -37,7 +37,7 @@ const Header = () => {
             </button>
           </Link>
         </div>
-        <div className="ml-20 space-y-5">
+        <div className="ml-4 sm:ml-20 space-y-5">
           <h1 className="text-xl md:text-4xl md:w-5/6 w-2/3 font-bold">
             Good food never fail in bringing people together
           </h1>
